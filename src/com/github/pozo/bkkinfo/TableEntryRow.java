e0384e4fe0f9ed97d8e6b08d8a3e91b6b9a71695
pozo <pozo@gmx.com>
2013-11-06 20:11:23 +0100

    removed unused import

--- src/com/github/pozo/bkkinfo/TableEntryRow.java
+++ src/com/github/pozo/bkkinfo/TableEntryRow.java
@@ -11,7 +11,6 @@ import android.util.DisplayMetrics;
 import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.LayoutInflater;
-import android.view.View;
 import android.view.ViewGroup.LayoutParams;
 import android.widget.LinearLayout;
 import android.widget.TableLayout;
