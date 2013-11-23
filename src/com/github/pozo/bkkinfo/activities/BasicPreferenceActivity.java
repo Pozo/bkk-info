8c45eb9825e1db38d29cd9cf25106cfb5cfd2c3c
pozo <pozo@gmx.com>
2013-11-23 10:49:53 +0100

    Added preference reading at onCreate and refactored service starting

--- src/com/github/pozo/bkkinfo/activities/BasicPreferenceActivity.java
+++ src/com/github/pozo/bkkinfo/activities/BasicPreferenceActivity.java
@@ -6,15 +6,25 @@ import com.github.pozo.bkkinfo.shared.Constants;
 import com.github.pozo.bkkinfo.shared.NetworkConnectionHelper;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
 import android.util.Log;
 
 public class BasicPreferenceActivity extends PreferenceActivity {
+	public static final String PREFERENCES_NOTIFICATION_SOUND = "notificationSound";
+	public static final String PREFERENCES_SYNC_DATE = "prefLastSyncDate";
+	
 	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.preferences);
+
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+        Preference prefLastSyncDate = findPreference(PREFERENCES_SYNC_DATE);
+        prefLastSyncDate.setSummary(sharedPrefs.getString(PREFERENCES_SYNC_DATE, getResources().getString(R.string.notification_settings_last_refresh_default_value)));
     }
 
 	@Override
@@ -23,7 +33,10 @@ public class BasicPreferenceActivity extends PreferenceActivity {
 		Log.i(Constants.LOG_TAG, "BasicPreferenceActivity:onPause");
 		if(NetworkConnectionHelper.isNetworkConnected(this)) {
 			stopService(new Intent(this, NotificationService.class));
-			startService(new Intent(this, NotificationService.class));			
+			
+			Intent intent = new Intent(this, NotificationService.class);
+			intent.putExtra(NotificationService.KEY_NEED_REFRESH, false);
+			startService(intent);
 		}
 	}
 }
